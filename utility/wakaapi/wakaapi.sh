deff426916ebc85687f9a03bd248348adfe846fd
ReachableCEO <reachableceo@turnsys.com>
2024-11-26 10:07:46 -0600

    pulling together the last few personal velocity multiplier apps

--- utility/wakaapi/wakaapi.sh
+++ utility/wakaapi/wakaapi.sh
@@ -4,5 +4,6 @@ docker run -d \
   -p 2001:3000 \
   -e "WAKAPI_PASSWORD_SALT=1234" \
   -v wakapi-data-tsys:/data \
+  --restart=always \
   --name wakaapi-tsys \
   ghcr.io/muety/wakapi:latest
---
+++ webapps/vscode-server/run.sh
@@ -0,0 +1,1 @@
+docker run -it --init -p 1003:3000 -v "tsys-vscode:/home/workspace:cached" gitpod/openvscode-server
