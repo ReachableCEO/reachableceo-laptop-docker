a48de1b0d429722d6839c502d20ee50a34b20a59
ReachableCEO <reachableceo@turnsys.com>
2024-11-26 10:26:21 -0600

    coming together nicely

--- utility/wakaapi/wakaapi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-  -p 2001:3000 \
-  -e "WAKAPI_PASSWORD_SALT=1234" \
-  -v wakapi-data-tsys:/data \
-  --restart=always \
-  --name wakaapi-tsys \
-  ghcr.io/muety/wakapi:latest
